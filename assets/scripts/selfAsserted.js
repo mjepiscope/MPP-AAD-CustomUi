ffba3b0d2d6cbda456815c2dc476dd395d901c66
Michael Episcope <mjepiscope@gmail.com>
2024-07-31 20:19:08 +0800

    add spinner when resending code for email verification

--- assets/scripts/selfAsserted.js
+++ assets/scripts/selfAsserted.js
@@ -2,7 +2,7 @@ import { handleRequest } from './emailVerificationStepIndicator.js';
 import { setSpinnerStyleDisplay, setupSpinnerTriggerButtons } from './spinner.js';
 
 setSpinnerStyleDisplay('none');
-setupSpinnerTriggerButtons('div.buttons', 'button#continue, button#readOnlyEmail_ver_but_send, button#readOnlyEmail_ver_but_verify');
+setupSpinnerTriggerButtons('div.buttons', 'button#continue, button#readOnlyEmail_ver_but_send, button#readOnlyEmail_ver_but_verify, button#readOnlyEmail_ver_but_resend');
 
 $(document).ajaxSend(function (e, jqXhr, settings) {
     if (settings.contentType.startsWith('application/x-www-form-urlencoded')) {
